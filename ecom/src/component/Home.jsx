75ae755bcfe17c5488b7a7fbc6d5994fcacf8f0b
Pappu Kumar <digitalpappu101@gmail.com>
2022-07-28 21:59:10 +0530

    day1

--- ecom/src/component/Home.jsx
+++ ecom/src/component/Home.jsx
@@ -1,5 +1,21 @@
+import axios from "axios";
 import React from "react";
-
 export const Home = () => {
+
+    const[products,setProducts]=React.useState({
+        loading:false,
+        error:false,
+        data:
+    });
+
+    React.useEffect(() => {
+      axios({
+        method:"get",
+        url:"https://localhost:8080/products"
+      }).then(res=>setProducts(res.data))
+      .catch(err=>)
+    
+      
+    }, [])
   return <div>Home</div>;
 };
